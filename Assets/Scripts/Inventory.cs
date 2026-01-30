7a0ff80b8db8007594cdb020632f4052ead6ce3e
Daniel González <chiki4089@gmail.com>
2026-01-30 22:33:40 +0100

    -Basic inventory management and scriptableobject for items.

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,44 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    private readonly HashSet<ItemData> _inventory = new HashSet<ItemData>();
+
+	public void AddItem(ItemData item)
+    {
+        if (!_inventory.Contains(item))
+        {
+            _inventory.Add(item);
+        }
+        else
+        {
+            Debug.LogError("[Inventory.AddItem] ERROR. Item " + item.Name + " already present");
+        }
+    }
+
+    public void UseItem(ItemData item)
+    {
+        if (_inventory.Contains(item))
+        {
+            // Use
+            RemoveItem(item);
+        }
+		else
+		{
+			Debug.LogError("[Inventory.UseItem] ERROR. Item " + item.Name + " not present");
+		}
+	}
+
+    private void RemoveItem(ItemData item)
+    {
+        if (_inventory.Contains(item))
+        {
+            _inventory.Remove(item);
+        }
+		else
+		{
+			Debug.LogError("[Inventory.RemoveItem] ERROR. Item " + item.Name + " not present");
+		}
+	}
+}
---
+++ Assets/Scripts/ScriptableObjects/ItemData.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Data/ItemData")]
+public class ItemData : ScriptableObject
+{
+    public string Name = "";
+    public Sprite Image = null;
+}
