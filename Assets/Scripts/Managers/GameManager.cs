55b9e6f5f70958a539efccb2d22d99a255efa780
Daniel González <chiki4089@gmail.com>
2026-01-30 18:35:37 +0100

    Basic

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+	private static GameManager _instance = null;
+
+	public static GameManager Instance => _instance;
+
+	private void Awake()
+	{
+		if (_instance == null)
+		{
+			_instance = this;
+			DontDestroyOnLoad(gameObject);
+			Init();
+		}
+		else
+		{
+			Destroy(gameObject);
+		}
+	}
+
+	private void Init()
+	{
+    }
+}
