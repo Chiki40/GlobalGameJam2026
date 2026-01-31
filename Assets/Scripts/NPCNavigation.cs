ddaa79d8765ed27d3fcd4eee9ae7dd7dd160b8df
Daniel González <chiki4089@gmail.com>
2026-01-31 10:42:05 +0100

    - NavMesh for NPCs.

---
+++ Assets/Scripts/NPCNavigation.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NPCNavigation : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _basePoint = null;
+	[SerializeField]
+    private float _purseTime = 5.0f;
+
+    private NavMeshAgent _agent = null;
+    private Transform _target = null;
+    private bool _pursuing = false;
+    private float _pursuingTime = 0.0f;
+
+	private void Awake()
+	{
+		_agent = GetComponent<NavMeshAgent>();
+	}
+
+    private IEnumerator Start()
+    {
+		GoToTarget(GameObject.FindGameObjectWithTag("Player").transform);
+        yield return new WaitForSeconds(10.0f);
+        PursueTarget(GameObject.FindGameObjectWithTag("Player").transform);
+	}
+
+	public void SetAgentEnabled(bool enabled)
+	{
+        _agent.isStopped = !enabled;
+	}
+
+	private void Update()
+	{
+        if (_target != null)
+        {
+            _agent.SetDestination(_target.position);
+        }
+
+		if (_pursuing)
+        {
+            _pursuingTime += Time.deltaTime;
+            if (_pursuingTime >= _purseTime)
+            {
+                GoToTarget(_basePoint);
+            }
+        }
+	}
+
+	public void GoToTarget(Transform target)
+    {
+        _target = target;
+        _pursuing = false;
+		_pursuingTime = 0.0f;
+	}
+
+    public void PursueTarget(Transform target)
+    {
+        _target = target;
+        _pursuing = true;
+		_pursuingTime = 0.0f;
+    }
+}
