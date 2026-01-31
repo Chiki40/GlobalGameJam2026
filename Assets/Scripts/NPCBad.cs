58f1cf9c0a6926f8d43f8b8785094453eaf8a42f
MiniPC <m.moracuesta@gmail.com>
2026-01-31 20:14:56 +0100

    soy tontito

--- Assets/Scripts/NPCBad.cs
+++ Assets/Scripts/NPCBad.cs
@@ -5,6 +5,9 @@ public class NPCBad : NPC
 	[SerializeField]
 	private float _purseTime = 5.0f;
 
+	[SerializeField]
+	private float _attackDistancec = 1.0f;
+
 	protected override void Awake()
 	{
 		base.Awake();
@@ -23,6 +26,10 @@ public class NPCBad : NPC
 				if (distance <= _detectionDistance)
 				{
 					_npcNavigation.PursueTarget(_playerMaskController.transform);
+				}
+				else if (distance <= _attackDistancec)
+				{
+					_playerMaskController.GetComponent<DamageComponent>().TakeDamage();
 				}
 			}
 			else if (_playerMaskController.IsMaskOn && _npcNavigation.IsPursuing(_playerMaskController.transform))
