bdb3987163226b866b5eec915d3d41d7dc4dfcce
Daniel González <chiki4089@gmail.com>
2026-02-01 14:37:48 +0100

    Aim

--- Assets/Scripts/Player/InteractionController.cs
+++ Assets/Scripts/Player/InteractionController.cs
@@ -10,6 +10,10 @@ public class InteractionController : MonoBehaviour
 	private LayerMask _interactionMask = default;
 	[SerializeField]
 	private Image _pointer = default;
+	[SerializeField]
+	private Color _aimNormalColor = Color.white;
+	[SerializeField]
+	private Color _aimDetectingColor = Color.red;
 
 	private StarterAssetsInputs _input = null;
 
@@ -23,6 +27,8 @@ public class InteractionController : MonoBehaviour
 #if DEBUG
 		Debug.DrawLine(Camera.main.transform.position, Camera.main.transform.position + Camera.main.transform.forward * _interactionDistance, Color.red);
 #endif
+		bool raycast = Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out RaycastHit hit, _interactionDistance, _interactionMask, QueryTriggerInteraction.Collide);
+		_pointer.color = raycast ? _aimDetectingColor : _aimNormalColor;
 
 		if (!_input.interact)
 		{
@@ -35,17 +41,22 @@ public class InteractionController : MonoBehaviour
 			return;
 		}
 
-		TryToInteract();
+		TryToInteract(raycast, hit);
 	}
 
-	private void TryToInteract()
+	private void TryToInteract(bool valid, RaycastHit hit)
 	{
-		if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out RaycastHit hit, _interactionDistance, _interactionMask, QueryTriggerInteraction.Collide))
+		if (valid)
 		{
+			_pointer.color = _aimDetectingColor;
 			if (hit.collider.TryGetComponent(out SmartObjectInteractable smartObject))
 			{
 				smartObject.TryToInteract();
 			}
+		}
+		else
+		{
+			_pointer.color = _aimNormalColor;
 		}
 	}
 }
